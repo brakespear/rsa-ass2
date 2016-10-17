33a2dbbd4c6feb229e57f4561f6b1dc139d4df08
Brontesaurus <bronte.kalebic@gmail.com>
2016-10-17 17:18:45 +1100

    added node and launch files for beacon markers

---
+++ src/nodes/beacon_markers_node.cpp
@@ -0,0 +1,22 @@
+#include <ros/ros.h>
+
+#include <cstdio>
+#include <cstdlib>
+
+#include <ass2/beaconMarkers.hpp>
+
+#define LOG_START  "Ass2BeaconNode ::"
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "beacon_node");
+
+	ros::NodeHandle nh("~");
+  beaconMarkers bm;
+	
+	while(ros::ok()) {
+		//ROS_INFO("%s spinning",LOG_START);
+		ros::spin();
+	}
+
+	return 0;
+}
