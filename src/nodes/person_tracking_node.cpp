427f88fff7061d5db0625c0bfb85b5b4fac0a563
Brontesaurus <bronte.kalebic@gmail.com>
2016-10-17 12:00:20 +1100

    linked up image converter correctly

--- src/nodes/person_tracking_node.cpp
+++ src/nodes/person_tracking_node.cpp
@@ -12,7 +12,7 @@ int main(int argc, char **argv) {
 	ros::init(argc, argv, "person_tracking_node");
 
 	ros::NodeHandle nh("~");
-	PolarLegDetector polarLegDetector;
+	PolarLegDetector polarLegDetector();
 	
 	while(ros::ok()) {
 		ROS_INFO("%s spinning",LOG_START);
